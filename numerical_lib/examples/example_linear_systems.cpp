a80003b5cd9388a0f6f45ba292e15bf569ced743
mikolaj <152820628+mikolaj11111111@users.noreply.github.com>
2025-06-13 01:08:58 +0200

    Update example_linear_systems.cpp

--- numerical_lib/examples/example_linear_systems.cpp
+++ numerical_lib/examples/example_linear_systems.cpp
@@ -3,9 +3,9 @@
 #include <vector>
 
 int main() {
-    std::cout << "=== Przyk³ad: Rozwi¹zywanie uk³adu równañ ===" << std::endl;
+    std::cout << "=== Przyklad: Rozwiazywanie ukladu rownan ===" << std::endl;
 
-    // Uk³ad: 2x + y = 3, x + 3y = 4
+    // UkÂ³ad: 2x + y = 3, x + 3y = 4
     std::vector<std::vector<double>> A = { {2, 1}, {1, 3} };
     std::vector<double> b = { 3, 4 };
 
@@ -14,7 +14,7 @@ int main() {
     auto b_copy = b;
     auto solution_gauss = NumLib::LinearSystems::gauss_elimination(A_copy, b_copy);
 
-    std::cout << "Rozwi¹zanie metod¹ Gaussa: ";
+    std::cout << "Rozwiazanie metoda Gaussa: ";
     for (double x : solution_gauss) {
         std::cout << x << " ";
     }
@@ -24,7 +24,7 @@ int main() {
     auto lu = NumLib::LinearSystems::lu_decomposition(A);
     auto solution_lu = NumLib::LinearSystems::solve_lu(lu, b);
 
-    std::cout << "Rozwi¹zanie metod¹ LU: ";
+    std::cout << "Rozwiazanie metoda LU: ";
     for (double x : solution_lu) {
         std::cout << x << " ";
     }
